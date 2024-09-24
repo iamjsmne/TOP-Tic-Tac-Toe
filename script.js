6e2c0efe8e203618bd4a6203408bb1bb15f9aa27
iamjsmne <87401508+iamjsmne@users.noreply.github.com>
2024-09-23 22:05:59 -0400

    Reworked Tic-Tac-Toe game code

--- script.js
+++ script.js
@@ -1,76 +1,70 @@
 const GameBoard = (() => {
-    let gameBoard = [];
-    //create board & it's grid *NEED THIS*
-    const rows = 3;
-    const cols = 3;
+    const gameBoard = Array(9).fill("");
+    //get gameboard array
+    const getGameBoard = () => gameBoard;
+    //create gameboard grid
+    let gridNo = 0;
 
-    for(let i = 0; i < rows; i++) { //row
-        gameBoard[i] = [];
-        for(let j = 0; j < cols; j++) {//cols
-            gameBoard[i].push(Grid().getValue());
-            getGameGrid();
-            
+    const createGameGrid= () => {
+        const gameGrid = document.createElement('div');
+        gameGrid.classList.add('game-cell');
+        gameGrid.setAttribute('id', gridNo);
+        document.getElementById('game-board').appendChild(gameGrid);
+        gridNo++;
+        return gameGrid;
+    }
+
+    gameBoard.forEach(() => createGameGrid());
+
+    const resetGameBoard = () => { //Linked with DisplayController.
+        function removeGameCell(name) {
+            const gameGridElement = document.getElementsByClassName(name);
+            //remove first element(index 0) always because wehn you remove element
+            //it also shrinks the elements array
+            while(gameGridElement.length > 0) {
+                gameGridElement[0].parentNode.removeChild(gameGridElement[0]);
+            }
         }
-    }
-    const getBoard = () => gameBoard;
-    
-    function getGameGrid() {
-        const gameGridElement = document.createElement('div');
-        gameGridElement.classList.add('gameGrid');
-        document.getElementById('gameBoard').appendChild(gameGridElement);
-        return gameGridElement;
+        removeGameCell('game-cell');
+        gameBoard.fill("");
+        gridNo = 0;
+        gameBoard.forEach(() => createGameGrid());
+        gameBoard.addEventListener('click', GameController.grids, true);
     }
 
-    return { getBoard};
+    console.log(gameBoard);
+    return {gameBoard, getGameBoard, createGameGrid, resetGameBoard}
 })();
-function Grid() {
-    let value = "";
-    let getValue = () => value;
-    let getMarker = (marker) => (value = marker);
-    return { getValue, getMarker };
-}
-function createPlayer(name, marker, score) {
-    return { name, marker, score}
-}
 
-const DisplayController = ()=> {
-    const messageElement = document.getElementById('message');
-    const resetBtnElement = document.getElementById('resetButton');
-    /*const player1Score = document.getElementById('player1Score');
-    const player2Score = document.getElementById('player2Score');*/
+const DisplayController = (() => {
+    const headerMessage = document.getElementById('header-message');
+    const resetBtn = document.getElementById('reset-button');
 
-    const setMessage = (message) => {
-        messageElement.textContent = message;
-    }
-    resetBtnElement.addEventListener('click', () => {
-        GameBoard.resetBoard();
-        alert("sup");
-    });
+    const changeMessage = (player) => { //works
+        headerMessage.textContent = `${player}'s turn!`;
+    };
 
-    return { setMessage }
-};
-
-/*
-const displayController = (() =>{
-    const gameGridElement = document.createElement('div');
-    gameGridElement.classList.add('gameGrid');
-    document.getElementById('gameBoard').appendChild(gameGridElement);
-    const gameGrid = document.getElementsByClassName('gameGridElement');
-    gameGridElement.addEventListener('click', (event)=> {
-        const { target } = event;
-        target.textContent = players[0].marker;
-        alert("hi");
+    resetBtn.addEventListener('click', () => {
+        GameBoard.resetGameBoard(); //linked
     })
 
-    return { gameGridElement };
-})();*/
+    return { headerMessage, changeMessage }
+})();
 
-function GameController() {
-    const player1 = createPlayer("Player 1", "O", 0);
-    const player2 = createPlayer("Player 2", "X", 0);
-    const players = [player1, player2];
+const createPlayer = (name,marker) => {
+    return {name, marker}; 
+}
+
+const GameController = (() => {
+    const gameBoard = document.getElementById('game-board');
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    const playerOne = createPlayer("Player One", "O");
+    const playerTwo = createPlayer("Player Two", "X");
+    const players = [playerOne, playerTwo];
     let currentPlayer = players[0];
-    let gameBoard = GameBoard;
+    
     const switchPlayer = () => {
         if(currentPlayer === players[0]) {
             currentPlayer = players[1];
@@ -79,7 +73,31 @@ function GameController() {
         }
     }
 
+    gameBoard.addEventListener('click', grids, true);
+    
+    function grids(e) {
+        let grid = e.target;
+        let gridId = grid.id;
+        if(grid.className == 'game-cell') {
+            if(grid.textContent === "") {
+                grid.textContent = currentPlayer.marker;
+                GameBoard.gameBoard[gridId] = currentPlayer.name;
+                updateGameBoard(gridId, currentPlayer.marker);
+                checkBoard();
+                switchPlayer();
+            }
+        }
+    }
+    const updateGameBoard = (gridNo, playerMarker) => {
+        GameBoard.gameBoard[gridNo] = playerMarker;
+        return GameBoard.getGameBoard;
+    }
+
     const checkBoard = () => {
+        //gameBoard
+        //['0'. '1', '2']
+        //['3', '4', '5']
+        //['6', '7', '8']
         const winningStats = [
             [0, 1, 2],
             [3, 4, 5],
@@ -90,42 +108,28 @@ function GameController() {
             [0, 4, 8],
             [2, 4, 6]
         ];
-        for(let winStat of winningStats) {
-            const [position1, position2, position3] = winStat;
-
-            if (
-                gameBoard[position1] !== '' &&
-                gameBoard[position1] === gameBoard[position2] &&
-                gameBoard[position1] === gameBoard[position3] 
-            ) {
-                alert(`${gameBoard[position1]}'s wins!`);
-                return;
-            }
-        }
-        const allGridMarked = gameBoard.every(grid => grid !== '');
-        if(allGridMarked) {
-            alert('It\'s a draw!');
-        }
-    }
-    const resetGame = () => {
-
-    }
-
-    document.addEventListener("click", function(event){
-        let grid = event.target;
-        if (grid.className =='gameGrid') {
-            if (grid.textContent === "") {
-                grid.textContent = currentPlayer.marker;
-                switchPlayer();
-            } else {
-                checkBoard();
-                return;
-            }
+        for(let winState of winningStats) {
+            const [position1, position2, position3] = winState;
             
+            if (
+                GameBoard.gameBoard[position1] !== '' && 
+                GameBoard.gameBoard[position1] === GameBoard.gameBoard[position2] && 
+                GameBoard.gameBoard[position1] === GameBoard.gameBoard[position3]
+            ) {
+                alert(`${GameBoard.gameBoard[position1]}'s wins!`);
+                gameBoard.removeEventListener('click', grids, true);
+                return;
+                 //this prevents allCellsUsed from alert it's a draw
+            }
+    
         }
-    });
-    return { switchPlayer, checkBoard }
-}
+        const allCellsUsed = GameBoard.gameBoard.every(cell => cell !== '');
+        if(allCellsUsed) {
+            alert(`It's a draw!`);
+        }
+    }
+    
+    const getCurrentPlayer = () => alert(currentPlayer.name); //testing purpose
 
-DisplayController();
-GameController();
+    return{ switchPlayer, getCurrentPlayer, updateGameBoard, checkBoard}
+})();
